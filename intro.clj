690e0c37a17f879cb0906953edb4787bd817ba1e
Thomas Down <thomas.a.down@gmail.com>
2015-01-21 21:33:20 +0000

    Cleanups and a few notes.

--- intro.clj
+++ intro.clj
@@ -3,9 +3,11 @@
 ;; **
 ;;; # Instaparse intro
 ;;; 
-;;; Welcome to gorilla :-)
+;;; This is a [Gorilla REPL](http://gorilla-repl.org/) worksheet.
 ;;; 
 ;;; Shift + enter evaluates code. Hit ctrl+g twice in quick succession or click the menu icon (upper-right corner) for more commands ...
+;;; 
+;;; Evaluate the namespace declaration before trying anything else!
 ;; **
 
 ;; @@
@@ -14,22 +16,72 @@
             [parsers.utils :refer :all]))
 ;; @@
 
+;; **
+;;; Building parsers
+;;; =========
+;;; 
+;;; Instaparse allows parses to be build from a set of "production rules" in EBNF format
+;; **
+
+;; @@
+(def ab
+  (insta/parser
+    "S = A* B*
+     A = 'a'
+     B = 'b'"))
+;; @@
+
+;; **
+;;; To parse a string, just use your parser as a function, e.g.:
+;; **
+
+;; @@
+(ab "aaabb")
+;; @@
+
+;; **
+;;; If the input doesn't match your grammar, you'll get back a map containing some reasonably intelligable error data, e.g.:
+;; **
+
+;; @@
+(ab "aaabba")
+;; @@
+
+;; @@
+(ab "aaaAbb")
+;; @@
+
+;; **
+;;; Grammars can be ambiguous 
+;; **
+
 ;; @@
 (def xyz
   (insta/parser
-    "S=(X|Y|Z)*
-     X='x'+
-     Y='y'+
-     Z='z'+"))
+    "S = (X|Y|Z)*
+     X = 'x'+
+     Y = 'y'+
+     Z = 'z'+"))
 ;; @@
 
 ;; @@
 (xyz "xyzzy")
 ;; @@
 
+;; **
+;;; The `parses` function allows you to see all possible parses.
+;; **
+
 ;; @@
 (insta/parses xyz "xyzzy")
 ;; @@
+
+;; **
+;;; Something a little more sophisticated
+;;; ===========================
+;;; 
+;;; Note that we're using regexps for some production rules.  This is an easy way to recognize character classes, and also convenient if you want "greedy" terminal productions to make your grammar unambiguous.
+;; **
 
 ;; @@
 (def minilisp
@@ -46,6 +98,10 @@
 ;; @@
 (minilisp "(foo 42)")
 ;; @@
+
+;; **
+;;; Angle brackets "hide" either the output of a production, or the production's label, in the parser output.
+;; **
 
 ;; @@
 (def minilisp2
@@ -67,12 +123,26 @@
 (minilisp2 "(foo (times 7 6))")
 ;; @@
 
+;; **
+;;; The default output is reminiscent of Hiccup format (there is an enlive-like format as well, if you prefer that).  There are quite a few Clojure tools that are helpful for working with this (clojure.walk, core.match, etc).
+;;; 
+;;; Instaparse provides a `transform` function  which makes writing simple transformers quite easy.
+;; **
+
+;; @@
+(def parsed-lisp (minilisp2 "(foo (times 7 6))"))
+
+(insta/transform 
+  {:symbol str}
+  parsed-lisp)
+;; @@
+
 ;; @@
 (insta/transform 
   {:symbol (comp symbol str)
    :number (comp parse-int str)
    :s-expr list}
-  (minilisp2 "(foo (times 7 6))"))
+  parsed-lisp)
 ;; @@
 
 ;; @@
--- star-maps.clj
+++ star-maps.clj
@@ -3,7 +3,14 @@
 ;; **
 ;;; # Simple star maps
 ;;; 
-;;; We have a bright star catalog in the file "stars.dat".
+;;; We have a bright star catalog in the file "stars.dat".  This consists of a series of fairly complex four-line records. that look like
+;;; 
+;;;     39801 2061 5.91953 7.40694 0.5 1.85 58      
+;;;     $\alpha$    # name
+;;;     ORI         # constellation
+;;;     M1-2Ia-Iab  # spectral class
+;;;     
+;;; Needless to say, we'll write a grammar!
 ;; **
 
 ;; @@
@@ -14,18 +21,14 @@
 ;; @@
 
 ;; @@
-(use 'parsers.utils :reload)
-;; @@
-
-;; @@
 (def stars-parser
   (insta/parser
    "stars = star*
     star = line1 <newline> label <newline> constellation <newline> spec-class <newline>
-    <line1> = hd-number <space> bsc-number <space> 
+    <line1> = (<'0'> | hd-number) <space> (<'0'> |bsc-number) <space> 
               ra <space> dec <space> 
               visual-brightness <space> bv-brightness <space> 
-              flam-number
+              (<'0'> | flam-number)
 
     hd-number = int
     bsc-number = int
@@ -41,7 +44,7 @@
 
     newline = #'\\n'
     space = ' '
-    <int> = #'-?[0-9]+'
+    <int> = #'[1-9][0-9]*'
     <float> = #'-?[0-9]+(\\.[0-9]+)?'
     <line> = #'[^\\n]*'"))
 ;; @@
@@ -50,26 +53,54 @@
 (def stars-tree (stars-parser (slurp "data/stars.dat")))
 ;; @@
 
+;; **
+;;; Let's take a look...
+;; **
+
 ;; @@
-(def xfrm-map
+(take 2 stars-tree)
+;; @@
+
+;; **
+;;; Again, we'll use `transform` to flatten some of the structure and parse the numbers.
+;; **
+
+;; @@
+(def star-xfrm-map
   {:stars             vector
    :star              (fn [& l] (into {} l))
    :hd-number         (fn [x] [:hd-number (parse-int x)])
    :visual-brightness (fn [x] [:visual-brightness (parse-double x)])
    :bv-brightness     (fn [x] [:bv-brightness (parse-double x)])
-   :flam-number       (fn [x] [:flam-number (parse-nonzero x)])
+   :flam-number       (fn [x] [:flam-number (parse-int x)])
    :bsc-number        (fn [x] [:bsc-number (parse-int x)])
    :ra                (fn [x] [:ra (parse-double x)])
    :dec               (fn [x] [:dec (parse-double x)])})
 ;; @@
 
 ;; @@
-(def stars (insta/transform xfrm-map stars-tree))
+(def stars (insta/transform star-xfrm-map stars-tree))
 ;; @@
+
+;; **
+;;; ...which gives us some fairly tractable-looking Clojure maps.
+;; **
+
+;; @@
+(first stars)
+;; @@
+
+;; **
+;;; Use the `star-map` function to get a simple orphographic projection (adjust `:dec0` and `:ra0` to rotate if you like.)
+;; **
 
 ;; @@
 (star-map stars {:dec0 20 :ra0 2.5})
 ;; @@
+
+;; **
+;;; Could looking at constellations help me get my bearings?
+;; **
 
 ;; @@
 (def stars-by-const (group-by :constellation stars))
@@ -80,26 +111,21 @@
 ;; @@
 
 ;; @@
-(star-map orion {:ra0 6})
+(star-map orion {:ra0 2 :dec0 20})
 ;; @@
+
+;; **
+;;; Fortunately, we've also got a set of "constellation lines", which probably come closer to what you normally think of when talking about constellations....
+;;; 
+;;;      ORI 19  ORI 34  ORI 24 ;
+;;;      ORI 53  ORI 50  ORI 46  ORI 34 ;
+;;;      ORI 50  ORI 58 ;
+;;;      
+;;; This format uses semicolons as record-terminators, and also allows '#" as a comment character.
+;; **
 
 ;; @@
 (def lines-file (slurp "data/lines.dat"))
-;; @@
-
-;; @@
-(def line-parser
-  (insta/parser
-    "F = L*
-     L = #'[^\\n]*\\n'"))
-;; @@
-
-;; @@
-(line-parser "aa a\nbbb\n")
-;; @@
-
-;; @@
-(count (line-parser lines-file))
 ;; @@
 
 ;; @@
@@ -127,20 +153,16 @@
 (take 2 consts)
 ;; @@
 
-;; @@
-(def orion-lines (filter identity (map (partial const-stars orion) consts)))
-;; @@
+;; **
+;;; Use the `:lines` option to the `star-map` function to draw any constellation lines which can be found in the stars you provide.
+;; **
 
 ;; @@
 (star-map orion {:ra0 6 :lines consts})
 ;; @@
 
 ;; @@
-(def all-lines (filter identity (map (partial const-stars stars) consts)))
-;; @@
-
-;; @@
-(star-map stars {:ra0 5 :dec 20 :lines consts})
+(star-map stars {:ra0 6 :dec -20 :lines consts})
 ;; @@
 
 ;; @@
